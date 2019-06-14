fcdc16daf4f522b1fa3f27332bd068685cc21e79
Thomas Weibel <thom@ikhono.net>
2019-06-15 00:13:32 +0200

    Update PDF generation scripts and added PDFs

--- session-03/handout.sh
+++ session-03/handout.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat presentation.handout.tex | rubber-pipe -m beamer --texpath=styles --into /tmp --pdf > handout.pdf
-pdfnup --paper a4paper --nup 1x2  handout.pdf --outfile handout-2up.pdf
+cat presentation.handout.tex | rubber-pipe -m beamer --texpath=styles --pdf > handout.pdf
+pdfnup --paper a4paper --nup 1x2 --no-landscape handout.pdf --outfile handout-2up.pdf
 pdfnup --paper a4paper --nup 2x2  handout.pdf --outfile handout-4up.pdf
